f3fbb59bf2a601c38d0aeb311734a9b2900129bc
Thomas <thomas.guillaume@efrei.net>
2020-05-24 21:37:51 +0200

    fix method selectFiveThemes

--- src/main/java/org/farmas/model/themes/Themes.java
+++ src/main/java/org/farmas/model/themes/Themes.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Themes {
 
-    private String[] arrayOfThemes;
+    private String[] themes;
     private int indicator;
 
     public Themes() {
         // TODO charger les thèmes
-        this.arrayOfThemes = new String[10];
+        this.themes = new String[10];
         this.indicator = -1;
     }
 
     public String[] getArrayOfThemes() {
-        return arrayOfThemes;
+        return themes;
     }
 
     public int getIndicator() {
@@ -28,19 +28,30 @@ public class Themes {
     public void modifyTheme(String theme, int indexTheme){
         this.getArrayOfThemes()[indexTheme] = theme;
     }
-
+    
     public String selectTheme(){
         do {
-            int randomIndex = (int) Math.round(Math.random() * arrayOfThemes.length + 1);
+            int randomIndex = (int) Math.round(Math.random() * themes.length + 1);
             if(randomIndex != this.indicator){
                 this.indicator = randomIndex;
-                return this.arrayOfThemes[randomIndex];
+                return this.themes[randomIndex];
             }
         }while(true);
     }
 
-    public ArrayList<String> selectFiveTheme(){
-        return null;
+    public String[] selectFiveTheme(){
+        String[] themes = new String[5];
+        ArrayList<Integer> randomNbPick = new ArrayList<>();
+        int randomNumber;
+        for (int i = 0; i < 5; i++) {
+            // pick a random number
+            do{
+                randomNumber = (int) (Math.random() * 10 + 1);
+            }while(randomNbPick.contains(randomNumber)); // check if previously taken
+            randomNbPick.add(randomNumber);
+            themes[i] = this.themes[randomNumber];
+        }
+        return themes;
     }
 
 }
